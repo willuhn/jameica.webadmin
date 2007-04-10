9655cde6699970c48149a9be98f99a138c5ec633
willuhn <willuhn>
2007-04-10 00:11:39 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/webadmin/server/HttpServiceImpl.java
+++ src/de/willuhn/jameica/webadmin/server/HttpServiceImpl.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica.webadmin/src/de/willuhn/jameica/webadmin/server/HttpServiceImpl.java,v $
- * $Revision: 1.1 $
- * $Date: 2007/04/09 17:12:01 $
+ * $Revision: 1.2 $
+ * $Date: 2007/04/10 00:11:39 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -13,12 +13,20 @@
 
 package de.willuhn.jameica.webadmin.server;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.rmi.RemoteException;
-import java.rmi.server.RMIClientSocketFactory;
-import java.rmi.server.RMIServerSocketFactory;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Properties;
 
+import Acme.Serve.Serve;
+import Acme.Serve.Serve.BasicAuthRealm;
+import de.willuhn.jameica.system.Application;
+import de.willuhn.jameica.system.Settings;
+import de.willuhn.jameica.webadmin.Plugin;
 import de.willuhn.jameica.webadmin.rmi.HttpService;
+import de.willuhn.logging.Logger;
+import de.willuhn.logging.LoggerOutputStream;
 
 
 /**
@@ -26,6 +34,7 @@ import de.willuhn.jameica.webadmin.rmi.HttpService;
  */
 public class HttpServiceImpl extends UnicastRemoteObject implements HttpService
 {
+  private Worker worker = null;
 
   /**
    * @throws RemoteException
@@ -40,8 +49,7 @@ public class HttpServiceImpl extends UnicastRemoteObject implements HttpService
    */
   public String getName() throws RemoteException
   {
-    // TODO Auto-generated method stub
-    return null;
+    return "Webadmin HTTP-Service";
   }
 
   /**
@@ -49,8 +57,7 @@ public class HttpServiceImpl extends UnicastRemoteObject implements HttpService
    */
   public boolean isStartable() throws RemoteException
   {
-    // TODO Auto-generated method stub
-    return false;
+    return !isStarted();
   }
 
   /**
@@ -58,8 +65,7 @@ public class HttpServiceImpl extends UnicastRemoteObject implements HttpService
    */
   public boolean isStarted() throws RemoteException
   {
-    // TODO Auto-generated method stub
-    return false;
+    return this.worker != null;
   }
 
   /**
@@ -67,8 +73,13 @@ public class HttpServiceImpl extends UnicastRemoteObject implements HttpService
    */
   public void start() throws RemoteException
   {
-    // TODO Auto-generated method stub
-
+    if (isStarted())
+    {
+      Logger.warn("service allready started, skipping request");
+      return;
+    }
+    this.worker = new Worker();
+    this.worker.start();
   }
 
   /**
@@ -76,15 +87,132 @@ public class HttpServiceImpl extends UnicastRemoteObject implements HttpService
    */
   public void stop(boolean arg0) throws RemoteException
   {
-    // TODO Auto-generated method stub
-
+    if (!isStarted())
+    {
+      Logger.warn("service not started, skipping request");
+      return;
+    }
+    try
+    {
+      this.worker.interrupt();
+    }
+    finally
+    {
+      this.worker = null;
+    }
   }
 
+  /**
+   * Worker-Thread fuer den Webserver.
+   */
+  private class Worker extends Thread
+  {
+    private Server server = null;
+    
+    /**
+     * ct.
+     */
+    private Worker()
+    {
+      super("[jameica.webadmin] http-worker");
+      setDaemon(true);
+    }
+
+    /**
+     * @see java.lang.Thread#run()
+     */
+    public void run()
+    {
+      try
+      {
+        Settings settings = Application.getPluginLoader().getPlugin(Plugin.class).getResources().getSettings();
+        Properties props = new Properties();
+        props.put("port",new Integer(settings.getInt("listener.port",Serve.DEF_PORT)));
+        props.put(Serve.ARG_NOHUP,Serve.ARG_NOHUP);
+
+        this.server = new Server(props);
+        this.server.addDefaultServlets(null);
+        this.server.serve();
+      }
+      catch (Exception e)
+      {
+        Logger.error("unable to start http-server",e);
+        this.server = null;
+      }
+    }
+
+    /**
+     * @see java.lang.Thread#interrupt()
+     */
+    public void interrupt()
+    {
+      try
+      {
+        if (this.server != null)
+        {
+          Logger.info("stopping http-server");
+          try
+          {
+            this.server.notifyStop();
+          }
+          catch (IOException ioe)
+          {
+            Logger.error("error while notifying sevrlets",ioe);
+          }
+          this.server.destroyAllServlets();
+        }
+      }
+      finally
+      {
+        super.interrupt();
+      }
+    }
+  }
+  
+  /**
+   * Ueberschrieben, um die Log-Ausgaben bequemer umzuleiten.
+   */
+  private class Server extends Serve
+  {
+    /**
+     * @see Acme.Serve.Serve#log(java.lang.String, java.lang.Throwable)
+     */
+    public void log(String message, Throwable throwable)
+    {
+      Logger.error(message,throwable);
+    }
+
+    /**
+     * @see Acme.Serve.Serve#log(java.lang.String)
+     */
+    public void log(String message)
+    {
+      Logger.info(message);
+    }
+
+    /**
+     * ct.
+     * @param params
+     */
+    private Server(Properties params)
+    {
+      super(params,new PrintStream(new LoggerOutputStream(Logger.getLevel())));
+      // TODO: Konstruktor von "BasicAuthRealm" ist "package default" und damit unsichtbar
+//      PathTreeDictionary pt = new PathTreeDictionary();
+//      BasicAuthRealm realm = new BasicAuthRealm("Jameica-Webadmin Login");
+//      realm.put("admin",Application.getCallback().getPassword());
+//      pt.put("/",realm);
+//      this.setRealms(pt);
+    }
+  }
 }
 
 
 /**********************************************************************
  * $Log: HttpServiceImpl.java,v $
+ * Revision 1.2  2007/04/10 00:11:39  willuhn
+ * *** empty log message ***
+ *
  * Revision 1.1  2007/04/09 17:12:01  willuhn
  * *** empty log message ***
  *
