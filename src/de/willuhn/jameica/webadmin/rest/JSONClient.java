268a3c12a71aa57ceed7d8a55e5eb23b24d3e977
willuhn <willuhn>
2008-06-20 15:06:09 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/webadmin/rest/JSONClient.java
+++ src/de/willuhn/jameica/webadmin/rest/JSONClient.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica.webadmin/src/de/willuhn/jameica/webadmin/rest/Attic/JSONClient.java,v $
- * $Revision: 1.1 $
- * $Date: 2008/06/16 23:27:28 $
+ * $Revision: 1.2 $
+ * $Date: 2008/06/20 15:07:12 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -20,8 +20,15 @@ import java.net.Authenticator;
 import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import de.willuhn.logging.Logger;
@@ -32,7 +39,14 @@ import de.willuhn.logging.Logger;
  */
 public class JSONClient
 {
-  public static String execute(String url, final String username, final String password) throws IOException
+  /**
+   * @param url
+   * @param username
+   * @param password
+   * @return json-Daten.
+   * @throws IOException
+   */
+  private static String execute(String url, final String username, final String password) throws IOException
   {
     try
     {
@@ -68,11 +82,43 @@ public class JSONClient
       throw new IOException("unable to execute JSON request");
     }
   }
+  
+  /**
+   * @param url
+   * @param username
+   * @param password
+   * @return
+   * @throws Exception
+   */
+  public static Collection asList(String url, String username, String password) throws Exception
+  {
+    JSONArray array = new JSONArray(execute(url,username,password));
+    ArrayList result = new ArrayList();
+    for (int i=0;i<array.length();++i)
+    {
+      result.add(toMap(array.getJSONObject(i)));
+    }
+    return result;
+  }
+  
+  public static Map toMap(JSONObject object) throws JSONException
+  {
+    Map map = new HashMap();
+    Iterator  keys = object.keys();
+    while (keys.hasNext()) {
+      String key = (String) keys.next();
+      map.put(key,object.get(key));
+    }
+    return map;
+  }
 }
 
 
 /**********************************************************************
  * $Log: JSONClient.java,v $
+ * Revision 1.2  2008/06/20 15:07:12  willuhn
+ * *** empty log message ***
+ *
  * Revision 1.1  2008/06/16 23:27:28  willuhn
  * @N JSON-Client-Code zum Abfragen der Daten von anderen Jameica-Servern
  *
